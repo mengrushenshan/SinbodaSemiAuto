c4f079e3cd75cdb6bd422921b0dd7177d69622b3
liyuyang004 <499015131@qq.com>
2024-05-24 16:15:54 +0800

    30点逻辑增加

---
+++ SinbodaSemiAuto/Sinboda.SemiAuto.Model/DataOperation/SemiAuto/Sin_Cell_DataOperation.cs
@@ -0,0 +1,14 @@
+﻿using Sinboda.Framework.Core.AbstractClass;
+using Sinboda.SemiAuto.Model.DatabaseModel.SemiAuto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinboda.SemiAuto.Model.DataOperation.SemiAuto
+{
+    public class Sin_Cell_DataOperation : EFDataOperationBase<Sin_Cell_DataOperation, Sin_Cell, Sin_DbContext>
+    {
+    }
+}
---
+++ SinbodaSemiAuto/Sinboda.SemiAuto.Model/DatabaseModel/SemiAuto/Sin_Cell.cs
@@ -0,0 +1,36 @@
+﻿using Sinboda.Framework.Core.AbstractClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinboda.SemiAuto.Model.DatabaseModel.SemiAuto
+{
+    /// <summary>
+    /// 点位表
+    /// </summary>
+    [Serializable]
+    public partial class Sin_Cell : EntityModelBase
+    {
+        /// <summary>
+        /// 三个点位序列 1：原点 2、3：计算辅助点
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// X坐标
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y坐标
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Z坐标
+        /// </summary>
+        public int Z { get; set; }
+    }
+}
