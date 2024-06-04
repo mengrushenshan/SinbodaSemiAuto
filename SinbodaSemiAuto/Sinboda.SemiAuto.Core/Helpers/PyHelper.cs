3c93a60db106f5ea027dee375527bf64c4ea99e6
Haijin-H <166678767+Haijin-H@users.noreply.github.com>
2024-06-04 21:20:55 +0800

    pythonnet + python3.10 + auto_focus.py
    1、PyHelper 实现 C# 调用 python脚本功能
    2、目前实现了 自动对焦脚本
    3、后面需要修改为调用 .pyc或.pyd文件

---
+++ SinbodaSemiAuto/Sinboda.SemiAuto.Core/Helpers/PyHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Python.Runtime;
+using System.IO;
+
+namespace Sinboda.SemiAuto.Core.Helpers
+{
+    public static class PyHelper
+    {
+        public static void Init()
+        {
+            string pathToVirtualEnv = ".\\Python310";
+            Runtime.PythonDLL = Path.Combine(pathToVirtualEnv, "python310.dll");
+            PythonEngine.PythonHome = Path.Combine(pathToVirtualEnv, "python.exe");
+            PythonEngine.PythonPath = $"./scripts;{pathToVirtualEnv}/DLLs;{pathToVirtualEnv}/Lib;{pathToVirtualEnv}/Lib/site-packages;";
+            PythonEngine.Initialize();
+        }
+
+        public static List<int> Autofocus()
+        {
+            List<int> list = new List<int>();
+            using (Py.GIL())
+            {
+                dynamic py = Py.Import("auto_focus");
+                PyList res = PyList.AsList(py.find_focused_img("png_folder", "E:/scripts/png_folder"));
+                list.Add((int)(dynamic)res[0]);
+                list.Add((int)(dynamic)res[1]);
+            }
+            return list;
+        }
+
+        public static void Shutdown()
+        { 
+            PythonEngine.Shutdown();
+        }
+    }
+}
