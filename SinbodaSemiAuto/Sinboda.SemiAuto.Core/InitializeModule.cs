85d8c841d731104f41e745b71cba3d1adb0c4298
shenxinyang <2576989461@qq.com>
2024-07-10 14:40:06 +0800

    登录界面退出时 释放相机资源

--- SinbodaSemiAuto/Sinboda.SemiAuto.Core/InitializeModule.cs
+++ SinbodaSemiAuto/Sinboda.SemiAuto.Core/InitializeModule.cs
@@ -19,6 +19,8 @@ namespace Sinboda.SemiAuto.Core
         public void FinalizeResource()
         {
             TcpCmdActuators.Instance.Dispose();
+            //释放相机资源
+            PVCamHelper.Instance.Dispose();
         }
 
         public List<ModuleMenuItem> GetMenus()
